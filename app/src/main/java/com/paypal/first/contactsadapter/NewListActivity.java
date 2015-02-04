a8e960b57c6219dfb8faeed47dc9a17643e8640f
Lange Pantoja <wpantoja@paypal.com>
2015-02-04 11:57:02 +0800

    Changing the elements of the list in the background

--- app/src/main/java/com/paypal/first/contactsadapter/NewListActivity.java
+++ app/src/main/java/com/paypal/first/contactsadapter/NewListActivity.java
@@ -1,11 +1,14 @@
 package com.paypal.first.contactsadapter;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NewListActivity extends Activity {
 
@@ -14,7 +17,7 @@ public class NewListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_list);
 
-        List<Person> listPerson = new ArrayList<>();
+        final List<Person> listPerson = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             Person person = new Person();
             person.setFirstName("first " + i);
@@ -24,9 +27,28 @@ public class NewListActivity extends Activity {
         }
 
         ListView listView = (ListView) findViewById(R.id.listView);
-        NewListAdapter adapter = new NewListAdapter(this, R.layout.newlistlayout, listPerson);
+        final NewListAdapter adapter = new NewListAdapter(this, R.layout.newlistlayout, listPerson);
         listView.setAdapter(adapter);
 
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Person person = new Person();
+                        long timestamp = System.currentTimeMillis();
+                        person.setFirstName("firstname_timer-" + timestamp);
+                        person.setLastName("lastname_timer-" + timestamp);
+                        person.setAge((int) timestamp % 50);
+                        listPerson.add(person);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }, 5000, 5000);
+
     }
+
 
 }
